257a730b2f12f0d6230e6e7e13cd69026e76708c
TimoCid <rodrigo.rojas.cabello@gmail.com>
2016-11-18 02:39:07 -0300

    versión con login de Facebook base
    utilizando código de internet

---
+++ config/auth.js
@@ -0,0 +1,20 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : '1843274765900588', // your App ID
+        'clientSecret'    : 'f74e11d48605289f19039e2a0bb7454b', // your App Secret
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : '566HmmtT4jy3c4kmPXWTraMtr',
+        'consumerSecret'     : 'vHyeKpLBzQVHMFfBYbF4BfNgLHZ90lRdUZIVXbpFXENi5eon9Z',
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+};
